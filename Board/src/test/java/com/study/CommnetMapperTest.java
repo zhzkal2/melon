49a7afb899e7be75d5d494494a55a8f74b3a91f2
DESKTOP-4Q5LA11\plo <zhzkal2@naver.com>
2023-08-14 21:47:40 +0900

    댓글 테스트 코드 주석 처리

--- Board/src/test/java/com/study/CommnetMapperTest.java
+++ Board/src/test/java/com/study/CommnetMapperTest.java
@@ -13,18 +13,18 @@ public class CommnetMapperTest {
     @Autowired
     CommentService commentService;
 
-    @Test
-    void save() {
-
-        CommentRequest params = new CommentRequest();
-        for( long i = 1; i < 101; i++ ) {
-            params.setId(i);
-            params.setContent("댓글 컨텐츠" + i);
-            params.setWriter("댓글 작성자" + i);
-            params.setPostId(32356L); // 마지막 게시물 지정
-            commentService.saveComment(params);
-        }
-
-
-    }
+//    @Test
+//    void save() {
+//
+//        CommentRequest params = new CommentRequest();
+//        for( long i = 103; i < 105; i++ ) {
+//            params.setId(i);
+//            params.setContent("댓글 컨텐츠" + i);
+//            params.setWriter("댓글 작성자" + i);
+//            params.setPostId(32356L); // 마지막 게시물 지정
+//            commentService.saveComment(params);
+//        }
+//
+//
+//    }
 }
